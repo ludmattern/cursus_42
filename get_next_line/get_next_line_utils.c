b8492f801ba1753a284e1594829bc0a984c9452f
ludmattern <github.rfgrg@passmail.net>
2023-11-24 15:05:19 +0100

    changed the join function and removed calloc and strdup

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -1,37 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 10:03:09 by lmattern          #+#    #+#             */
-/*   Updated: 2023/11/22 13:48:00 by lmattern         ###   ########.fr       */
+/*   Updated: 2023/11/24 15:00:26 by lmattern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+size_t	ft_strlen(char *s)
 {
-	unsigned char	*ptr;
-	size_t			max_allocatable;
-	size_t			total_size;
+	size_t	i;
 
-	max_allocatable = (size_t)-1;
-	if (!nmemb || !size)
-		total_size = 0;
-	else
-	{
-		if (nmemb > max_allocatable / size)
-			return (NULL);
-		total_size = nmemb * size;
-	}
-	ptr = malloc(total_size);
-	if (!ptr)
-		return (NULL);
-	ft_bzero(ptr, total_size);
-	return (ptr);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
 }
 
 char	*ft_strchr(const char *s, int c)
@@ -41,24 +29,6 @@ char	*ft_strchr(const char *s, int c)
 	if (*s == (unsigned char)c)
 		return ((char *)s);
 	return (NULL);
-}
-
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	s1_len;
-	size_t	s2_len;
-	char	*str;
-
-	if (!s1 || !s2)
-		return (NULL);
-	s1_len = ft_strlen(s1);
-	s2_len = ft_strlen(s2);
-	str = malloc((s1_len + s2_len + 1) * sizeof(char));
-	if (!str)
-		return (NULL);
-	ft_strlcpy(str, s1, s1_len + 1);
-	ft_strlcpy(str + s1_len, s2, s2_len + 1);
-	return (str);
 }
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
@@ -81,31 +51,30 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	return (src_len);
 }
 
-size_t	ft_strlen(const char *s)
+char *ft_strjoin(char *s1, char *s2)
 {
-	size_t	i;
+    char *str;
+    char *original_s1;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*str;
-	size_t	s_len;
-
-	if (!s)
-		return (NULL);
-	s_len = ft_strlen(s);
-	if (start >= s_len)
-		return (ft_strdup(""));
-	if (len > s_len - start)
-		len = s_len - start;
-	str = ft_calloc(len + 1, sizeof(char));
-	if (!str)
-		return (NULL);
-	ft_strlcpy(str, s + start, len + 1);
-	return (str);
+	original_s1 = s1;
+    if (!s1)
+        s1 = ft_strdup("");
+    if (!s1 || !s2)
+    {
+        if (original_s1 != s1)
+            free(s1);
+        return (NULL);
+    }
+    str = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+    if (!str)
+    {
+        if (original_s1 != s1)
+            free(s1);
+        return (NULL);
+    }
+    ft_strlcpy(str, s1, ft_strlen(s1) + 1);
+    ft_strlcpy(str + ft_strlen(s1), s2, ft_strlen(s2) + 1);
+    if (original_s1 != s1)
+        free(s1);
+    return (str);
 }
