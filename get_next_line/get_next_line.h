34d7bb6857f159780528068ccefea3c3c5cac32e
lmattern <ludmattern@gmail.com>
2023-11-24 15:06:33 +0100

    Update get_next_line.h removing unused functions

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -17,11 +17,9 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
