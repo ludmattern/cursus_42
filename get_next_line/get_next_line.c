eee1300618f2389d8100583dc9ba3112ee76fada
lmattern <ludmattern@gmail.com>
2023-11-25 22:29:23 +0100

    Update get_next_line.c

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../include/get_next_line.h"
+#include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
