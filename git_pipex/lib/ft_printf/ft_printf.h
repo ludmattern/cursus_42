f4d627f41f8529c61a15aeff3c953ceb1f92660d
lmattern <ludmattern@gmail.com>
2024-03-21 14:23:43 +0100

    pipex

--- git_pipex/lib/ft_printf/ft_printf.h
+++ git_pipex/lib/ft_printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 14:09:08 by lmattern          #+#    #+#             */
-/*   Updated: 2024/01/22 19:30:30 by lmattern         ###   ########.fr       */
+/*   Updated: 2024/03/06 13:13:05 by lmattern         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,10 @@
 
 int		ft_printf(const char *format, ...);
 char	*itoa_printf(unsigned long long num, int base);
-int		print_num(unsigned int num, char fmt);
-int		print_char(char c);
-int		print_string(char *str);
-int		print_pointer(void *ptr, char *str, int len);
-int		print_int(int nbr);
+int		print_num_printf(unsigned int num, char fmt);
+int		print_char_printf(char c);
+int		print_string_printf(char *str);
+int		print_pointer_printf(void *ptr, char *str, int len);
+int		print_int_printf(int nbr);
 
 #endif
