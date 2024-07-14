1358f5a70109c5fb7ecdd8311a37fa0ffc953b73
ludmattern <github.rfgrg@passmail.net>
2024-07-14 16:10:30 +0200

    executed the mariadb initialisation in the Dockerfile foreground

--- git_inception/srcs/requirements/wordpress/tools/create_wordpress.sh
+++ git_inception/srcs/requirements/wordpress/tools/create_wordpress.sh
@@ -6,10 +6,10 @@ then
     echo "wordpress already downloaded"
 else
     # Télécharger WordPress
-    php -r "file_put_contents('wordpress-6.4.tar.gz', file_get_contents('https://wordpress.org/wordpress-6.4.tar.gz', false, stream_context_create(['ssl' => ['verify_peer' => false, 'verify_peer_name' => false]])));"
-    tar xfz wordpress-6.4.tar.gz
+    php -r "file_put_contents('wordpress-6.5.5.tar.gz', file_get_contents('https://wordpress.org/wordpress-6.5.5.tar.gz', false, stream_context_create(['ssl' => ['verify_peer' => false, 'verify_peer_name' => false]])));"
+    tar xfz wordpress-6.5.5.tar.gz
     mv wordpress/* .
-    rm -rf wordpress-6.4.tar.gz
+    rm -rf wordpress-6.5.5.tar.gz
     rm -rf wordpress
 
     # Importer les variables d'environnement dans le fichier de configuration
@@ -37,7 +37,7 @@ else
 
     # Attendre que la base de données soit prête
     echo "Waiting for MySQL to be ready..."
-    sleep 10
+    sleep 5
 
     # Remplir la base de données avec les tables WordPress
     php -r "
@@ -45,6 +45,7 @@ else
     require_once 'wp-load.php';
     require_once 'wp-admin/includes/upgrade.php';
     wp_install('$WORDPRESS_TITLE', '$WORDPRESS_ADMIN_USER', '$WORDPRESS_ADMIN_EMAIL', true, '', '$WORDPRESS_ADMIN_PASSWORD');
+    wp_create_user('$WORDPRESS_USER', '$WORDPRESS_USER_PASSWORD', '$WORDPRESS_USER_EMAIL');
     " 2>/dev/null
 	echo "Wordpress ready."
 fi
