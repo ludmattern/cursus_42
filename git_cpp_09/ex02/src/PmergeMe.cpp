b7a80ce33adb100738dd002235d2ab7ea9fe0b17
ludmattern <github.rfgrg@passmail.net>
2024-07-28 20:59:26 +0200

    normed the cpp09

--- git_cpp_09/ex02/src/PmergeMe.cpp
+++ git_cpp_09/ex02/src/PmergeMe.cpp
@@ -2,35 +2,115 @@
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   PmergeMe.cpp                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
+/*                                                    +:+ +:+         +:+    */
 /*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/24 16:45:23 by lmattern          #+#    #+#             */
-/*   Updated: 2024/07/27 17:41:44 by lmattern         ###   ########lyon.fr   */
+/*   Updated: 2024/07/28 12:35:44 by lmattern         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "PmergeMe.hpp"
+#include <iostream>
 #include <algorithm>
+#include <cerrno>
+#include <limits>
 
-template <typename T>
-void mergeInsertSortImpl(T& container);
+PmergeMe::PmergeMe() {}
 
-void mergeInsertSort(std::vector<int>& vec) {
-    mergeInsertSortImpl(vec);
+PmergeMe::PmergeMe(int argc, char** argv)
+{
+    input = parseArguments(argc, argv);
 }
 
-void mergeInsertSort(std::list<int>& lst) {
-    mergeInsertSortImpl(lst);
+PmergeMe::PmergeMe(const PmergeMe& other) : input(other.input) {}
+
+PmergeMe& PmergeMe::operator=(const PmergeMe& other)
+{
+    if (this != &other) input = other.input;
+    return *this;
 }
 
-template <typename T>
-void mergeInsertSortImpl(T& container) {
-    if (container.size() <= 1) {
-        return;
+PmergeMe::~PmergeMe() {}
+
+std::vector<int> PmergeMe::parseArguments(int argc, char** argv)
+{
+    std::vector<int>    vec;
+    char*               endptr;
+    long                num;
+    int                 i;
+
+    for (i = 1; i < argc; ++i)
+    {
+        errno = 0;
+        num = std::strtol(argv[i], &endptr, 10);
+        
+        if (*endptr != '\0')
+            throw std::runtime_error("Invalid input: Non-integer value");
+        if (errno == ERANGE || num > std::numeric_limits<int>::max() || num < std::numeric_limits<int>::min())
+            throw std::runtime_error("Invalid input: Integer overflow");
+        if (num <= 0)
+            throw std::runtime_error("Invalid input: Non-positive integer");
+
+        vec.push_back(static_cast<int>(num));
     }
+    return vec;
+}
 
-    typename T::iterator mid = container.begin();
+void PmergeMe::sortAndMeasure()
+{
+    std::vector<int>    vec;
+    double              vectorTime, listTime, timeDifference;
+    std::list<int>      lst(input.begin(), input.end());
+
+    vec = input;
+    vectorTime = measureSortTime(vec);
+    listTime = measureSortTime(lst);
+
+    std::cout << "Before: ";
+    printContainer(input);
+
+    std::cout << "After: ";
+    printContainer(vec);
+
+    std::cout << "Time to process a range of " << vec.size() << " elements with std::vector: " << vectorTime << " µs" << std::endl;
+    std::cout << "Time to process a range of " << lst.size() << " elements with std::list: " << listTime << " µs" << std::endl;
+
+    timeDifference = vectorTime - listTime;
+    std::cout << "Difference in time: " << timeDifference << " µs (positive means vector was slower, negative means list was slower)" << std::endl;
+}
+
+template <typename Container> double PmergeMe::measureSortTime(Container& container)
+{
+    std::clock_t    start, end;
+    double          delta;
+
+    start = std::clock();
+    mergeInsertSortImpl(container);
+    end = std::clock();
+    delta = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1e6;
+
+    return delta;
+}
+
+template <typename Container> void PmergeMe::printContainer(const Container& container)
+{
+    typename Container::const_iterator it;
+
+    for (it = container.begin(); it != container.end(); ++it)
+        std::cout << *it << " ";
+
+    std::cout << std::endl;
+}
+
+template <typename T> void PmergeMe::mergeInsertSortImpl(T& container)
+{
+    typename T::iterator    mid, leftIt, rightIt;
+
+    if (container.size() <= 1)
+        return;
+
+    mid = container.begin();
     std::advance(mid, container.size() / 2);
     T left(container.begin(), mid);
     T right(mid, container.end());
@@ -39,26 +119,20 @@ void mergeInsertSortImpl(T& container) {
     mergeInsertSortImpl(right);
 
     container.clear();
-    typename T::iterator leftIt = left.begin();
-    typename T::iterator rightIt = right.begin();
+    leftIt = left.begin();
+    rightIt = right.begin();
 
-    while (leftIt != left.end() && rightIt != right.end()) {
-        if (*leftIt < *rightIt) {
-            container.push_back(*leftIt);
-            ++leftIt;
-        } else {
-            container.push_back(*rightIt);
-            ++rightIt;
-        }
+    while (leftIt != left.end() && rightIt != right.end())
+    {
+        if (*leftIt < *rightIt)
+            container.push_back(*leftIt++);
+        else
+            container.push_back(*rightIt++);
     }
 
-    while (leftIt != left.end()) {
-        container.push_back(*leftIt);
-        ++leftIt;
-    }
+    while (leftIt != left.end())
+        container.push_back(*leftIt++);
 
-    while (rightIt != right.end()) {
-        container.push_back(*rightIt);
-        ++rightIt;
-    }
+    while (rightIt != right.end())
+        container.push_back(*rightIt++);
 }
--- git_cpp_09/ex02/src/main.cpp
+++ git_cpp_09/ex02/src/main.cpp
@@ -6,80 +6,33 @@
 /*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 09:57:48 by lmattern          #+#    #+#             */
-/*   Updated: 2024/07/27 18:04:58 by lmattern         ###   ########lyon.fr   */
+/*   Updated: 2024/07/28 20:46:24 by lmattern         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ostream>
 #include <iostream>
-#include <vector>
-#include <list>
-#include <cstdlib>
-#include <ctime>
 #include "PmergeMe.hpp"
 
-int main(int argc, char** argv) {
-    if (argc < 2) {
+int main(int argc, char** argv)
+{
+    if (argc < 2)
+    {
         std::cerr << "Error: No input provided" << std::endl;
         return 1;
     }
 
-    std::vector<int> vec;
-    std::list<int> lst;
+    try
+    {
+        PmergeMe    pmerge(argc, argv);
 
-    clock_t start = clock();
-    try {
-        for (size_t i = 1; i < static_cast<size_t>(argc); ++i) {
-            int num = std::atoi(argv[i]);
-            if (num <= 0) {
-                throw std::runtime_error("Invalid input: Non-positive integer");
-            }
-            vec.push_back(num);
-        }
-    } catch (const std::exception& e) {
+        pmerge.sortAndMeasure();
+    }
+    catch (const std::exception& e)
+    {
         std::cerr << "Error: " << e.what() << std::endl;
         return 1;
     }
-    mergeInsertSort(vec);
-    clock_t end = clock();
-    double vectorTime = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1e6;
-
-    start = clock();
-    try {
-        for (size_t i = 1; i < static_cast<size_t>(argc); ++i) {
-            int num = std::atoi(argv[i]);
-            if (num <= 0) {
-                throw std::runtime_error("Invalid input: Non-positive integer");
-            }
-            lst.push_back(num);
-        }
-    } catch (const std::exception& e) {
-        std::cerr << "Error: " << e.what() << std::endl;
-        return 1;
-    }
-    mergeInsertSort(lst);
-    end = clock();
-    double listTime = static_cast<double>(end - start) / CLOCKS_PER_SEC * 1e6;
-
-    std::cout << "Before: ";
-    for (size_t i = 1; i < static_cast<size_t>(argc); ++i) {
-        std::cout << argv[i] << " ";
-    }
-    std::cout << std::endl;
-
-    std::cout << "After: ";
-    for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it) {
-        std::cout << *it << " ";
-    }
-    std::cout << std::endl;
-
-    std::cout << "Time to process a range of " << vec.size() << " elements with std::vector: " 
-              << vectorTime << " µs" << std::endl;
-
-    std::cout << "Time to process a range of " << lst.size() << " elements with std::list: " 
-              << listTime << " µs" << std::endl;
-
-    double timeDifference = vectorTime - listTime;
-    std::cout << "Difference in time: " << timeDifference << " µs (positive means vector was faster, negative means list was faster)" << std::endl;
 
     return 0;
 }
