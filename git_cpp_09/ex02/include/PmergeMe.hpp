bc0a32a0226217277f81e2cbb46c09af735ab567
ludmatternm <ludmattern@gmail.com>
2024-07-28 20:59:26 +0200

    normed the cpp09

--- git_cpp_09/ex02/include/PmergeMe.hpp
+++ git_cpp_09/ex02/include/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: lmattern <lmattern@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 09:37:28 by lmattern          #+#    #+#             */
-/*   Updated: 2024/07/27 17:42:35 by lmattern         ###   ########lyon.fr   */
+/*   Updated: 2024/07/28 20:51:44 by lmattern         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,25 @@
 
 #include <vector>
 #include <list>
+#include <ctime>
+#include <stdexcept>
 
-void mergeInsertSort(std::vector<int>& vec);
-void mergeInsertSort(std::list<int>& lst);
+class PmergeMe
+{
+public:
+    PmergeMe();
+    PmergeMe(int argc, char** argv);
+    PmergeMe(const PmergeMe& other);
+    PmergeMe& operator=(const PmergeMe& other);
+    ~PmergeMe();
+    void sortAndMeasure();
+
+private:
+    std::vector<int> parseArguments(int argc, char** argv);
+    template <typename Container> double measureSortTime(Container& container);
+    template <typename Container> void printContainer(const Container& container);
+    template <typename Container> void mergeInsertSortImpl(Container& container);
+    std::vector<int> input;
+};
 
 #endif
